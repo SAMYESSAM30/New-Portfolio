0f448c30b811bd9a7b9802929e66afc1fc8c91ba
SAMYESSAM30 <samyessam2000@gmail.com>
2024-12-27 17:36:06 +0200

    updade

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -7,7 +7,7 @@ const Data = () => {
   return (
     <div className="home__data">
       <h1 className="home__title">
-        Samy Essam
+        {t("samy-essam")}{" "}
         <svg
           width="36"
           height="36"
